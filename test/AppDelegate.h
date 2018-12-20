cc4239c169e35ac4a9b3bf6478b01c05da39662d
xuanhu <892863526@qq.com>
2018-12-20 16:32:36 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 邓梦超 on 2018/12/20.
+//  Copyright © 2018 邓梦超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 邓梦超 on 2018/12/20.
+//  Copyright © 2018 邓梦超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
