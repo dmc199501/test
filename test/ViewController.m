94342e72e4f4e3d6c1308bc0e0b9dc2c212ba9ff
dmc <892863526@qq.com>
2020-05-30 18:11:22 +0800

    ceshi

--- test/ViewController.m
+++ test/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
     ///////kkdkkad
     // Do any additional setup after loading the view, typically from a nib.
 }
